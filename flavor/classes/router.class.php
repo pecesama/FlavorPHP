5d0f7d5940f5465d4e40693383d17ed1badd66b7
Victor De la Rocha <vyk2rr@gmail.com>
2009-01-24 22:14:52 +0000

    bug fixes on routing class
    git-svn-id: http://svn.flavorphp.com/flavor@25 871cedec-e7be-43c2-9e7a-5e46dfd0203a

--- flavor/classes/router.class.php
+++ flavor/classes/router.class.php
@@ -1,7 +1,5 @@
 <?php
-
-class router {
-	
+class router{
 	private $registry;
 
 	public function __construct() {
@@ -10,14 +8,16 @@ class router {
 
 	public function dispatch() {
 		
-		$this->getController($controller, $action, $params);
+		$this->getController($controller, $action, $params, $extra);
 		$class = $controller."_controller";
 
 		$controller = new $class();
 
 		if(is_callable(array($controller, $params))){
-			$action = $params;
-			$params = null;
+			if(!is_numeric($params)){
+				$action = $params;
+				$params = $extra?$extra:null;
+			}
 		}
 
 		$controller->action = $action;
@@ -28,37 +28,52 @@ class router {
 			$controller->$action();
 	}
 	
-	private function getController(&$controller, &$action, &$params) {
-		$params = NULL;
+	private function getController(&$controller, &$action, &$params, &$extra) {
+		$extra = null;
+		$params = null;
 		$route = (empty($_GET["url"])) ? "" : $_GET["url"];
 
-		if (empty($route)) { $route = "index"; }
+		if(empty($route))$route = "index";
 
 		$route = trim($route, "/\\");
 		$parts = explode("/", $route);
-		
-		if (isset($parts[0])) {
-			if (is_numeric($parts[0])) {
+
+		if (isset($parts[0])){
+			if (is_numeric($parts[0])){
 				$controller = "index";
 				$action = "index";
-				$params = $parts[0];	
-			} else {
-				if(!file_exists(Absolute_Path.'app'.DIRSEP.'controllers'.DIRSEP."{$parts[0]}_controller.php")){ 
-					$controller = "index";
-					$action = "index";
-					$params = $parts[0];
-				}else{
+				$params = $parts[0];
+			}else{
+				if(file_exists(Absolute_Path.'app'.DIRSEP.'controllers'.DIRSEP."{$parts[0]}_controller.php")){
 					$controller = $parts[0];
 
-					if (isset($parts[1])) {
-						$action = $parts[1];
-					} else {
-						$action = "index";
-					}
-
-					if (isset($parts[2])) {
-						$params = $parts[2];
-					}
+					if(isset($parts[1])){
+						if(is_numeric($parts[1])){
+							$action = 'index';
+							$params = $parts[1];
+						}else{
+							$action = $parts[1];
+							$params = isset($parts[2])?$parts[2]:null;
+						}	
+					}else
+						$action = 'index';
+				}else{
+					$controller = "index";
+					/*
+					 * action:index por defecto.
+					 * Al tener una instancia, se debe verificar si existe el metodo/action dentro
+					 * para entonces convertir $action=$parts[0] y enviar como parametros los datos
+					 * contenidos en $parts[1]
+					 */
+					$action = "index";
+					$params = $parts[0];
+					/*
+					 * extra debe ser enviado, porque cuando se deduce que controller es index y action index,
+					 * el primer valor de parts[0] se envia params, y parts[1] se pierde. El cual es necesario
+					 * para enviar como params en caso de que parts[0] sea un action cuando creemos una
+					 * instancia de la clase $controller.
+					 */
+					$extra = isset($parts[1])?$parts[1]:null;
 				}
 			}
 		} else {
@@ -71,6 +86,5 @@ class router {
 		header('Content-Type: text/html; charset=utf-8');
 		die("404 Controller action Not Found");
 	}
-
 }
 ?>
