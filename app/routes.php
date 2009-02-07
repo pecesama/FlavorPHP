36a2d3d8c9006c151f12f59e08e6f59012863c74
Victor De la Rocha <vyk2rr@gmail.com>
2009-02-07 08:39:58 +0000

    ...
    git-svn-id: http://svn.flavorphp.com/flavor@36 871cedec-e7be-43c2-9e7a-5e46dfd0203a

--- app/routes.php
+++ app/routes.php
@@ -18,7 +18,4 @@
 	Tambien se puede mandar como segundo parametro un array como el siguiente:
 	$this->add('(.*)',array('controller'=>'index','action'=>'index','params'=>'$1'));	
 */
-$this->add('dos/uno/(.*)','cuatro/index/$1');
-$this->add('dos/(.*)','tres/index/$1');
-$this->add('(.*)','index/index/$1');
 ?>
