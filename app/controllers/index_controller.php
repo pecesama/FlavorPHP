37ee8b1fa36d5aa9cac4def07eeae45984725b1a
Victor De la Rocha <vyk2rr@gmail.com>
2011-02-11 19:32:17 -0600

    test: index_controller

--- app/controllers/index_controller.php
+++ app/controllers/index_controller.php
@@ -7,8 +7,24 @@ class Index_controller extends AppController{
 	}
 	
 	public function index($a=null,$b=null,$c=null,$d=null){		
-		header("content-type: text/plain");
-		echo "<<";
+		echo $a . "\n";
+		echo $b . "\n";
+		echo $c . "\n";
+		echo $d . "\n";
+		
+		$this->render();
+	}
+	
+	public function beforeDispatch(){
+		echo "beforeDispatch\n";
+	}
+	
+	public function beforeRender(){
+		echo "beforeRender\n";
+	}
+	
+	public function afterRender(){
+		echo "afterRender\n";
 	}
 }
 
