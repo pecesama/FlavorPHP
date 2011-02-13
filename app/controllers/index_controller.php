2c4483878eed8a0c295f7ce7e9921d9fa9b9943c
Victor De la Rocha <vyk2rr@gmail.com>
2011-02-12 22:52:55 -0600

    minor changes

--- app/controllers/index_controller.php
+++ app/controllers/index_controller.php
@@ -6,25 +6,8 @@ class Index_controller extends AppController{
 		parent::__construct();
 	}
 	
-	public function index(){
+	public function index($nombre = null){
 		
-		$this->redirect("index/test",true,null);
-		
-		$this->render();
-	}
-	
-	public function beforeDispatch(){
-		//echo "beforeDispatch\n";
-	}
-	
-	public function beforeRender($output){
-		//We can modify the output before $this->render() send the content to screen
-		$output = strip_tags($output);
-		$output = trim($output);
-	}
-	
-	public function afterRender(){
-		//echo "afterRender\n";
 	}
 }
 
