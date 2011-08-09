fb61b5e293892d603bd86e1aaa6d80797292f6ce
Aaron Munguia <aamm89@gmail.com>
2011-08-09 00:48:43 -0500

    implements reflection on singleton class with variants params in construction of a inheritance class

--- flavor/classes/singleton.class.php
+++ flavor/classes/singleton.class.php
@@ -3,9 +3,19 @@
 class Singleton {
 	private static $instances = array();
 	
-	public static function getInstance($class) {
+	public static function getInstance($class, $params = array()) {
 		if (!isset(self::$instances[$class])) {
-			self::$instances[$class] = new $class();
+                    $reflector = new ReflectionClass($class);
+                    
+//                    $parameters = $reflector->getMethod('__construct')->getParameters();
+//                    $parameters = $reflector->getMethod($class)->getParameters();
+//                    if(count($parameters) > 0){
+//                        self::$instances[$class] = $reflector->newInstanceArgs();
+//                    } else {
+//                        self::$instances[$class] = $reflector->newInstanceArgs($params);
+//                    }
+                    
+                    self::$instances[$class] = $reflector->newInstanceArgs($params);
 		}
 		return self::$instances[$class];
 	}
